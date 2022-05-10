c2d7bc6d7500699fdf48e55ed7310780c37d5802
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-05-10 04:33:09 -0500

    Worked on Traversals & Search

--- src/BinaryTree/BinaryTree.java
+++ src/BinaryTree/BinaryTree.java
@@ -31,4 +31,56 @@ public class BinaryTree {
         }
     }
 
+    public void inOrderTraverseTree(TreeNode focusNode){
+        if(focusNode != null){
+            inOrderTraverseTree(focusNode.left);
+            System.out.println(focusNode);
+            inOrderTraverseTree(focusNode.right);
+        }
+    }
+
+    public void preOrderTraverseTree(TreeNode focusNode){
+        if(focusNode != null){
+            System.out.println(focusNode);
+            preOrderTraverseTree(focusNode.left);
+            preOrderTraverseTree(focusNode.right);
+        }
+    }
+
+    public void postOrderTraverseTree(TreeNode focusNode){
+        if(focusNode != null){
+            postOrderTraverseTree(focusNode.left);
+            postOrderTraverseTree(focusNode.right);
+            System.out.println(focusNode);
+        }
+    }
+
+    public TreeNode searchTree(int id){
+        TreeNode searchNode = root;
+        while(searchNode.id != id){
+            if(id < searchNode.id){
+                searchNode = searchNode.left;
+            }else{
+                searchNode = searchNode.right;
+            }
+
+            if(searchNode == null) return null;
+        }
+        return searchNode;
+    }
+
+    public static void main(String[] args){
+        BinaryTree newTree = new BinaryTree();
+        newTree.addNode(50, "Osaretin");
+        newTree.addNode(10, "Victoria");
+        newTree.addNode(75, "Ade");
+        newTree.addNode(100, "Boye");
+        newTree.addNode(7, "Sammy");
+
+        newTree.postOrderTraverseTree(newTree.root);
+
+        System.out.println("Searching For Victoria");
+        System.out.println(newTree.searchTree(10));
+    }
+
 }
