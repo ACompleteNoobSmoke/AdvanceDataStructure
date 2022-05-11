b140ba48b5d8ed68fb31fd1e523ec9ba5aaecfab
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-05-11 02:26:37 -0500

    Worked on removal

--- src/BinaryTree/BinaryTree.java
+++ src/BinaryTree/BinaryTree.java
@@ -49,6 +49,82 @@ public class BinaryTree{
         return null;
     }
 
+    public boolean remove(int id){
+        if(root != null){
+            TreeNode focusNode = root;
+            TreeNode parentNode = root;
+            boolean isItALeftChild = true;
+
+            while(focusNode.id != id){
+                parentNode = focusNode;
+                if(id < focusNode.id){
+                    isItALeftChild = true;
+                    focusNode = focusNode.left;
+                }else{
+                    isItALeftChild = false;
+                    focusNode = focusNode.right;
+                }
+                if(focusNode == null) return false;
+            }
+
+            if(focusNode.left == null && focusNode.right == null){
+                if(focusNode == root){
+                    root = null;
+                }else if(isItALeftChild){
+                    parentNode.left = null;
+                }else{
+                    parentNode.right = null;
+                }
+            }else if(focusNode.right == null){
+                if(focusNode == root){
+                    root = focusNode.left;
+                }else if(isItALeftChild){
+                    parentNode.left = focusNode.left;
+                }else{
+                    parentNode.right = focusNode.left;
+                }
+            }else if(focusNode.left == null){
+                if(focusNode == root){
+                    root = focusNode.right;
+                }else if(isItALeftChild){
+                    parentNode.left = focusNode.right;
+                }else{
+                    parentNode.right = focusNode.right;
+                }
+            }else{
+                TreeNode replacement = getReplacement(focusNode);
+                if(focusNode == root){
+                    root = replacement;
+                }else if(isItALeftChild){
+                    parentNode.left = replacement;
+                }else{
+                    parentNode.right = replacement;
+                }
+                replacement.left = focusNode.left;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public TreeNode getReplacement(TreeNode replacedNode){
+        TreeNode replacementParent = replacedNode;
+        TreeNode replacement = replacedNode;
+
+        TreeNode focusNode = replacedNode.right;
+
+        while(focusNode != null){
+            replacementParent = replacement;
+            replacement = focusNode;
+            focusNode = focusNode.left;
+        }
+
+        if(replacement != replacedNode.right){
+            replacementParent.left = replacement.left;
+            replacement.right = replacedNode.right;
+        }
+        return replacement;
+    }
     /***** FUNCTIONALITY(End) *****/
 
     /***** TRAVERSALS(Start) *****/
@@ -92,7 +168,10 @@ public class BinaryTree{
         //testTree.inOrderTraversal(testTree.root);
         //testTree.preOrderTraversal(testTree.root);
         //testTree.postOrderTraversal(testTree.root);
-        System.out.println(testTree.search(100));
+        //System.out.println(testTree.search(100));
+
+        testTree.remove(5);
+        testTree.inOrderTraversal(testTree.root);
     }
 
 
