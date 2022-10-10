28f6e37ecb48d4ab15292fcd8c7e4f1e0347b9f9
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-10-10 14:13:59 -0500

    Worked on the push functionality

--- src/test/java/Stack/StackTest.java
+++ src/test/java/Stack/StackTest.java
@@ -1,80 +1,52 @@
 package Stack;
 
-import BinaryTree.Person;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class StackTest {
+public class StackTest {
 
-    private StackMethods stacks;
-    private List<Person> testPeople;
-
+    public Stack underTest;
 
     @BeforeEach
-    void setUp() {
-        testPeople = getTestPeople();
-        int limit = testPeople.size();
-        stacks = new Stack(limit);
+    public void setUp(){
+        underTest = new Stack(5);
     }
 
-    public List<Person> getTestPeople(){
-        List<Person> people = new ArrayList<>();
-        people.add(new Person(50, "Victoria", "Freelancer"));
-        people.add(new Person(10, "Osaretin", "Sailor"));
-        people.add(new Person(100, "Boye", "Programmer"));
-        people.add(new Person(85, "Ade", "Programmer"));
-        people.add(new Person(20, "Sammy", "Student"));
-        people.add(new Person(35, "Armando", "Gamer Designer"));
-        return people;
+    public Books[] getBooksArray(){
+        Books theStand = new Books("The Stand", "Stephen King", 1988);
+        Books bourneIdentity = new Books("The Bourne Identity", "Robert Ludlum", 1980);
+        Books goosebumps = new Books("Goosebumps", "R.L Stine", 2001);
+        Books othello = new Books("Othello", "William Shakespeare", 1601);
+        Books perksWallflower = new Books("The Perks of Being a Wallflower", "Stephen Chbosky", 1999);
+        return new Books[]{theStand, bourneIdentity, goosebumps, othello, perksWallflower};
     }
 
     @Test
-    public void pushOnePerson(){
-        int index = new Random().nextInt(testPeople.size());
-        Person newPerson = testPeople.get(index);
-        System.out.println("Index: " + index + "\n" + newPerson);
-        assertTrue(stacks.push(newPerson));
+    public void testPopFunction(){
+        Books theStand = getBooksArray()[0];
+        boolean result = underTest.push(theStand);
+        underTest.viewAll();
+        assertTrue(result);
     }
 
     @Test
-    public void pushMultiplePeople(){
-        testPeople.forEach(p -> stacks.push(p));
-        stacks.printAll();
-        assertEquals(stacks.getLimit(), testPeople.size());
+    public void testPopFunction2(){
+       for(int i = 0; i < 2; i++)
+           underTest.push(getBooksArray()[i]);
+        underTest.viewAll();
+        assertEquals(2, underTest.getCurrentSize());
     }
 
     @Test
-    public void cantPushIfFull(){
-        testPeople.forEach(p -> stacks.push(p));
-        Person newPerson =
-                new Person(123, "Omoruyi", "Programmer");
-        boolean added = stacks.push(newPerson);
-        stacks.printAll();
-        assertFalse(added);
+    public void testPopFunction3(){
+        Arrays.stream(getBooksArray()).forEach(underTest::push);
+        Books monster = new Books("Monster", "Walter Dean Myers", 1999);
+        boolean bookPushed = underTest.push(monster);
+        underTest.viewAll();
+        assertFalse(bookPushed);
     }
-
-    @Test
-    public void popOnePerson(){
-        testPeople.forEach(p -> stacks.push(p));
-        Person lastPerson = stacks.peek();
-        Person removedPerson = stacks.pop();
-        stacks.printAll();
-        assertTrue(lastPerson.equals(removedPerson));
-    }
-
-    @Test
-    public void popMultiplePeople(){
-        testPeople.forEach(p -> stacks.push(p));
-        while(!stacks.isEmpty()) stacks.pop();
-        stacks.printAll();
-        assertEquals(0, stacks.getSize());
-    }
-
-
 }
