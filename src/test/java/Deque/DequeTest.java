5a83923a0caab8f432d56751019510ea2b85b516
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-10-17 04:24:46 -0500

    Testing Insert Front Functionality(Deque)

--- src/test/java/Deque/DequeTest.java
+++ src/test/java/Deque/DequeTest.java
@@ -1,2 +1,26 @@
-package Deque;public class DequeTest {
+package Deque;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DequeTest {
+
+    private int capacity = 5;
+    private PracticeQueue<Integer> underTest;
+
+    @BeforeEach
+    public void setUp(){
+        underTest = new PracticeQueue<>(capacity);
+    }
+
+    @Test
+    public void testInsertFront1(){
+        boolean added = false;
+        for(int i = 1; i <= 5; i++){
+            added = underTest.insertFront(i);
+            assertTrue(added);
+        }
+        underTest.viewAll();
+    }
 }
