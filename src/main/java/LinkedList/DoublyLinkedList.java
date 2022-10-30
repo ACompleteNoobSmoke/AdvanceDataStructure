33d099c3acbb6a0496a9fbbceb69ad8821f2349c
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-10-30 12:14:21 -0500

    Practied Doubly Linked List

--- src/main/java/LinkedList/DoublyLinkedList.java
+++ src/main/java/LinkedList/DoublyLinkedList.java
@@ -1,10 +1,10 @@
 package LinkedList;
 
-public class LinkedList<T>{
+public class DoublyLinkedList<T> {
     private Node<T> head;
     private int size;
 
-    public LinkedList(){
+    public DoublyLinkedList(){
         head = null;
         size = 0;
     }
@@ -17,36 +17,38 @@ public class LinkedList<T>{
         return index <= 0 || index > size;
     }
 
-    private boolean isDataNull(T newData){
-        return newData == null;
+    private boolean isObjectNull(Object data){
+        return data == null;
     }
 
     public void insertHead(T newData){
-        if(isDataNull(newData)) throw new IllegalArgumentException("Data Is Null");
-        head = new Node<T>(newData, head);
+        if(isObjectNull(newData)) throw new IllegalArgumentException("Data Is Null");
+        if(isEmpty()) head = new Node<>(newData);
+        else head = new Node<T>(newData, head);
         size++;
     }
 
     public void insertTail(T newData){
-        if(isDataNull(newData)) throw new IllegalArgumentException("Data Is Null");
-        if(isEmpty()){ insertHead(newData); return; }
-        if(size == 1) head.setNext(new Node<>(newData));
+        if(isObjectNull(newData)) throw new IllegalArgumentException("Data Is Null");
+        if(isEmpty()) { insertHead(newData); return; }
+        if(size == 1) head.setNext(new Node<T>(head, newData));
         else{
             Node<T> current = head;
-            while(current.getNext() != null) current = current.getNext();
-            current.setNext(new Node<>(newData));
+            while(!isObjectNull(current.getNext())) current = current.getNext();
+            current.setNext(new Node<T>(current, newData));
         }
         size++;
     }
 
     public void insertAtIndex(int index, T newData){
-        if(isDataNull(newData)) throw new IllegalArgumentException("Data Is Null");
+        if(isObjectNull(newData)) throw new IllegalArgumentException("Data Is Null");
         if(isEmpty() || index > size + 1) return;
-        if(index == 1){ insertHead(newData); return; }
-        if(index == size + 1){ insertTail(newData); return; }
+        if(index == 1) { insertHead(newData); return; }
+        if(index == size + 1) { insertTail(newData); return; }
         Node<T> current = head;
-        for(int i = 1; i < index - 1; i++) current = current.getNext();
-        current.setNext(new Node<T>(newData, current.getNext()));
+        for(int i = 1; i < index; i++) current = current.getNext();
+        Node<T> newNode = new Node<T>(current.getPrev(), newData, current);
+        current.setPrev(newNode);
         size++;
     }
 
@@ -62,11 +64,11 @@ public class LinkedList<T>{
         if(isEmpty()) return null;
         if(size == 1) return removeHead();
         Node<T> current = head;
-        while (current.getNext().getNext() != null) current = current.getNext();
-        T removedTailData = current.getNext().getData();
-        current.setNext(null);
+        while(!isObjectNull(current.getNext())) current = current.getNext();
+        current.getPrev().setNext(null);
+        current.setPrev(null);
         size--;
-        return removedTailData;
+        return current.getData();
     }
 
     public T removeAtIndex(int index){
@@ -74,13 +76,12 @@ public class LinkedList<T>{
         if(index == 1) return removeHead();
         if(index == size) return removeTail();
         Node<T> current = head;
-        for(int i = 1; i < index - 1; i++) current = current.getNext();
-        T removedData = current.getNext().getData();
-        Node<T> replacementNode = current.getNext().getNext();
-        current.getNext().setNext(null);
-        current.setNext(replacementNode);
+        for(int i = 1; i < index; i++) current = current.getNext();
+        current.getPrev().setNext(current.getNext());
+        current.getNext().setPrev(current.getPrev());
+        current.setPrev(null); current.setNext(null);
         size--;
-        return removedData;
+        return current.getData();
     }
 
     public Node<T> searchNode(int index){
@@ -90,7 +91,7 @@ public class LinkedList<T>{
         else if(index == 2) found = head.getNext();
         else{
             Node<T> current = head;
-            for(int i = 1; i < index; i++ ) current = current.getNext();
+            for(int i = 1; i < index; i++) current = current.getNext();
             found = current;
         }
         return found;
@@ -106,14 +107,14 @@ public class LinkedList<T>{
         if(size == 1) return viewHead();
         if(size == 2) return head.getNext().getData();
         Node<T> current = head;
-        while(current.getNext() != null) current = current.getNext();
+        while(!isObjectNull(current.getNext())) current = current.getNext();
         return current.getData();
     }
 
     public void viewAll(){
         if(isEmpty()) return;
         Node<T> current = head;
-        while(current.getNext() != null){
+        while(!isObjectNull(current.getNext())){
             System.out.println(current.getData());
             current = current.getNext();
         }
--- src/main/java/LinkedList/Node.java
+++ src/main/java/LinkedList/Node.java
@@ -2,18 +2,35 @@ package LinkedList;
 
 public class Node<T> {
     private T data;
-    private Node<T> next;
+    private Node<T> prev, next;
 
     public Node(T data){
-        this(data, null);
+        this(null, data, null);
     }
 
     public Node(T data, Node<T> next){
+        this.prev = null;
+        this.data = data;
+        this.next = next;
+    }
+
+    public Node(Node<T> prev, T data){
+        this.prev = prev;
+        this.data = data;
+        this.next = null;
+    }
+
+    public Node(Node<T> prev, T data, Node<T> next){
+        this.prev = prev;
         this.data = data;
         this.next = next;
     }
 
     public T getData(){ return data; }
+
+    public void setPrev(Node<T> prev){ this.prev = prev; }
+
+    public Node<T> getPrev(){ return prev; }
 
     public void setNext(Node<T> next){ this.next = next; }
 
