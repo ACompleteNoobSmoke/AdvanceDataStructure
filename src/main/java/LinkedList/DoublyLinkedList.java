66eadfe5ec89e94ea54cad70a6b8e81e251f7eb9
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-11-13 11:00:48 -0600

    Practiced Doubly Linked List

--- src/main/java/LinkedList/DoublyLinkedList.java
+++ src/main/java/LinkedList/DoublyLinkedList.java
@@ -9,34 +9,34 @@ public class DoublyLinkedList<T>{
         size = 0;
     }
 
-    private boolean isEmpty(){ return head == null || size == 0; }
+    private boolean isEmpty(){ return isNull(head); }
 
-    private boolean isObjectNull(Object object) { return object == null; }
+    private boolean isNull(Object obj){ return obj == null; }
 
     private boolean isOutOfRange(int index){ return index <= 0 || index > size; }
 
     private boolean isOutOfRange2(int index){ return index <= 0 || index > size + 1; }
 
-    public void insertHead(T headData){
-        if(isObjectNull(headData)) throw new IllegalArgumentException("Data Is Null");
-        head = new Node<T>(null, headData, head);
+    public void insertHead(T newData){
+        if(isNull(newData)) throw new IllegalArgumentException("Data Is Null");
+        head = new Node<T>(null, newData, head);
         size++;
     }
 
-    public void insertTail(T tailData){
-        if(isObjectNull(tailData)) throw new IllegalArgumentException("Data Is Null");
-        if(isEmpty()){ insertHead(tailData); return; }
-        if(size == 1) head.setNext(new Node<T>(head, tailData));
+    public void insertTail(T newData){
+        if(isNull(newData)) throw new IllegalArgumentException("Data Is Null");
+        if(isEmpty()) { insertHead(newData); return; }
+        if(size == 1) head.setNext(new Node<T>(head, newData, null));
         else{
-            Node<T> current = head.getNext();
-            while(!isObjectNull(current.getNext())) current = current.getNext();
-            current.setNext(new Node<T>(current, tailData));
+            Node<T> current = head;
+            while(!isNull(current.getNext())) current = current.getNext();
+            current.setNext(new Node<T>(current, newData, null));
         }
         size++;
     }
 
     public void insertAtIndex(int index, T newData){
-        if(isObjectNull(newData)) throw new IllegalArgumentException("Data Is Null");
+        if(isNull(newData)) throw new IllegalArgumentException("Data Is Null");
         if(isEmpty() || isOutOfRange2(index)) return;
         if(index == 1) insertHead(newData);
         else if(index == size + 1) insertTail(newData);
@@ -61,8 +61,8 @@ public class DoublyLinkedList<T>{
     public T removeTail(){
         if(isEmpty()) return null;
         if(size == 1) return removeHead();
-        Node<T> current = head.getNext();
-        while(!isObjectNull(current.getNext())) current = current.getNext();
+        Node<T> current = head;
+        while(!isNull(current.getNext())) current = current.getNext();
         current.getPrev().setNext(null);
         current.setPrev(null);
         size--;
@@ -99,20 +99,18 @@ public class DoublyLinkedList<T>{
     public T viewTail(){
         if(isEmpty()) return null;
         if(size == 1) return viewHead();
-        if(size == 2) return head.getNext().getData();
-        Node<T> current = head.getNext();
-        while(!isObjectNull(current.getNext())) current = current.getNext();
+        Node<T> current = head;
+        while(!isNull(current.getNext())) current = current.getNext();
         return current.getData();
     }
 
     public void viewAll(){
         if(isEmpty()) return;
         Node<T> current = head;
-        while(!isObjectNull(current.getNext())){
+        while(!isNull(current)){
             System.out.println(current.getData());
             current = current.getNext();
         }
-        System.out.println(current.getData());
     }
 
     public int getSize(){ return size; }
