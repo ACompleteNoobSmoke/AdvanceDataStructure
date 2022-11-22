6905750e3d8a3360b29b1a081e08b283ddb7a3b6
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-11-22 01:04:06 -0600

    Worked on Hash Map

--- src/main/java/HashMap/MyPracticeHashMap.java
+++ src/main/java/HashMap/MyPracticeHashMap.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 import java.util.Objects;
 
 public class MyPracticeHashMap<K, V>{
-    private int capacity;
     private int size;
+    private int capacity;
     private SecondEntry<K, V>[] table;
 
     public MyPracticeHashMap(int capacity){
@@ -16,10 +16,24 @@ public class MyPracticeHashMap<K, V>{
 
     private boolean isEmpty(){ return size == 0; }
     private boolean isFull(){ return size == table.length; }
-    private boolean isNull(Object... obj){ return Arrays.stream(obj).anyMatch(Objects::isNull); }
+    private boolean isNull(Object... objs){ return Arrays.stream(objs).anyMatch(Objects::isNull); }
+
+    private void resize(int length){
+        int newLength = 2 * length + 1;
+        SecondEntry<K, V>[] oldTable = table;
+        table = new SecondEntry[newLength];
+        size = 0;
+
+        for(SecondEntry<K, V> entry: oldTable){
+            while(!isNull(entry)){
+                put(entry.getKey(), entry.getValue());
+                entry = entry.getNextEntry();
+            }
+        }
+    }
 
     public void put(K key, V value){
-        if(isNull(key, value)) throw new IllegalArgumentException("Key or Value is Null");
+        if(isNull(key, value)) throw new IllegalArgumentException("Key or Value Is Null");
         if(isFull()) resize(table.length);
         int hashIndex = key.hashCode() % table.length;
         if(hashIndex < 0) return;
@@ -31,18 +45,17 @@ public class MyPracticeHashMap<K, V>{
                 if(entry.getKey().equals(key)) { entry.setValue(value); return; }
                 entry = entry.getNextEntry();
             }
-            if(entry.getKey().equals(key)) { entry.setValue(value); return; }
+            if(entry.getKey().equals(key)){ entry.setValue(value); return; }
             entry.setNextEntry(new SecondEntry<>(key, value));
         }
         size++;
     }
 
     public V getValue(K key){
-        if(isEmpty()) return null;
         if(isNull(key)) throw new IllegalArgumentException("Key Is Null");
         int hashIndex = key.hashCode() % table.length;
-        if(hashIndex < 0) return null;
-        SecondEntry<K,V> searchEntry = table[hashIndex];
+        if(isEmpty() || hashIndex < 0) return null;
+        SecondEntry<K, V> searchEntry = table[hashIndex];
 
         while(!isNull(searchEntry)){
             if(searchEntry.getKey().equals(key)) return searchEntry.getValue();
@@ -53,10 +66,9 @@ public class MyPracticeHashMap<K, V>{
     }
 
     public SecondEntry<K, V> remove(K key){
-        if(isEmpty()) return null;
         if(isNull(key)) throw new IllegalArgumentException("Key Is Null");
         int hashIndex = key.hashCode() % table.length;
-        if(hashIndex < 0) return null;
+        if(isEmpty() || hashIndex < 0) return null;
         SecondEntry<K, V> removeEntry = table[hashIndex];
         if(isNull(removeEntry)) return null;
 
@@ -78,23 +90,10 @@ public class MyPracticeHashMap<K, V>{
                 return removeEntry;
             }
             previousEntry = removeEntry;
-            removeEntry = removeEntry.getNextEntry();
+            removeEntry = previousEntry.getNextEntry();
         }
+
         return null;
-    }
-
-    private void resize(int length){
-        int newLength = 2 * length + 1;
-        SecondEntry<K, V> oldTable[] = table;
-        table = new SecondEntry[newLength];
-        size = 0;
-
-        for(SecondEntry<K, V> entry: oldTable){
-            while(!isNull(entry)){
-                put(entry.getKey(), entry.getValue());
-                entry = entry.getNextEntry();
-            }
-        }
     }
 
     public SecondEntry<K, V>[] getTable(){ return table; }
