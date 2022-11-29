9a885795649b06840bd55a996b87219fba97bbf0
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-11-29 04:51:24 -0600

    Completed Helper Method Portion of AVL

--- src/main/java/AVL/AVLPractice.java
+++ src/main/java/AVL/AVLPractice.java
@@ -9,6 +9,18 @@ public class AVLPractice<T extends Comparable<T>>{
     private boolean isNull(Object obj){ return obj == null; }
     private boolean isLeftSide(T newData, T data){ return data.compareTo(newData) > 0; }
     private boolean isRightSide(T newData, T data){ return data.compareTo(newData) < 0; }
+    private int height(AVLNode<T> focusNode){ return isNull(focusNode) ? 0 : focusNode.getHeight(); }
+    private int balance(AVLNode<T> focusNode){
+        return isNull(focusNode) ? 0 : height(focusNode.getLeftSide()) - height(focusNode.getRightSide());
+    }
+
+    public void updateHeight(AVLNode<T> focusNode){
+        int maxHeight = Math.max(height(focusNode.getLeftSide()),
+                height(focusNode.getRightSide()));
+        focusNode.setHeight(maxHeight + 1);
+    }
+
+
 
     public T getMin(){
         if(isEmpty()) return null;
@@ -31,6 +43,7 @@ public class AVLPractice<T extends Comparable<T>>{
             return getMax(focusNode.getRightSide());
         return focusNode.getData();
     }
+
 
     public void insert(T newData){
         if(isNull(newData)) throw new IllegalArgumentException("Data Is Null");
