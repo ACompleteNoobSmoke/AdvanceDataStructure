daec119fbbd651ee78221dceee4419f73c8a174e
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-11-29 04:33:51 -0600

    Completed Helper Method Portion of AVL

--- src/main/java/AVL/AVLPractice.java
+++ src/main/java/AVL/AVLPractice.java
@@ -10,6 +10,28 @@ public class AVLPractice<T extends Comparable<T>>{
     private boolean isLeftSide(T newData, T data){ return data.compareTo(newData) > 0; }
     private boolean isRightSide(T newData, T data){ return data.compareTo(newData) < 0; }
 
+    public T getMin(){
+        if(isEmpty()) return null;
+        return getMin(root);
+    }
+
+    private T getMin(AVLNode<T> focusNode){
+        if(!isNull(focusNode.getLeftSide()))
+            return getMin(focusNode.getLeftSide());
+        return focusNode.getData();
+    }
+
+    public T getMax(){
+        if(isEmpty()) return null;
+        return getMax(root);
+    }
+
+    private T getMax(AVLNode<T> focusNode){
+        if(!isNull(focusNode.getRightSide()))
+            return getMax(focusNode.getRightSide());
+        return focusNode.getData();
+    }
+
 
     public void preOrderTraversal(AVLNode<T> focusNode){
         if(!isNull(focusNode)){
