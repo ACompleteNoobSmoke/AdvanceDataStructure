de087032fa7269f32dbd4d1e4cbd1cc1d293d964
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-12-02 09:49:17 -0600

    Test Binary Search

---
+++ src/test/java/BinarySearch/BinarySearchTest.java
@@ -0,0 +1,28 @@
+package BinarySearch;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BinarySearchTest {
+    private BinarySearch underTest;
+    int capacity = 10;
+
+    @BeforeEach
+    public void setUp(){
+        underTest = new BinarySearch<Integer>(capacity);
+        insertData();
+    }
+
+    private void insertData(){
+        for(int i = 1; i <= capacity; i++)
+            underTest.insertData(i);
+    }
+
+    @Test
+    public void testSearch(){
+        Integer target = 5;
+        Integer result = (Integer) underTest.searchData(target);
+        assertEquals(target, result);
+    }
+}
