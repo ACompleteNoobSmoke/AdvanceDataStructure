93360ce4acb503a5aca3c88b8ff66964f9d106a8
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-12-04 08:11:49 -0600

    Completed Binary Search

--- src/main/java/BinarySearch/BinarySearch.java
+++ src/main/java/BinarySearch/BinarySearch.java
@@ -1,49 +1,46 @@
 package BinarySearch;
 
 public class BinarySearch<T extends Comparable<T>>{
-    private T[] dataArray;
+    private T[] table;
     private int size;
 
     public BinarySearch(int capacity){
         this.size = 0;
-        this.dataArray = (T[]) new Comparable[capacity];
+        this.table = (T[]) new Comparable[capacity];
     }
 
     private boolean isEmpty(){ return size == 0; }
-    private boolean isFull(){ return size == dataArray.length; }
-    private boolean isNull(Object obj){ return obj == null; }
+    private boolean isFull(){ return size == table.length; }
 
     public void insertData(T newData){
-        if(isNull(newData)) throw new IllegalArgumentException("Data Is Null");
+        if(newData == null) throw new IllegalArgumentException("Data Is Null");
         if(isFull()) return;
-        dataArray[size++] = newData;
+        table[size++] = newData;
     }
 
-    public T searchData(T searchData){
-        if(isNull(searchData)) throw new IllegalArgumentException("Data Is Null");
+    public T searchData(T data){
+        if(data == null) throw new IllegalArgumentException("Data Is Null");
         if(isEmpty()) return null;
         int startIndex = 0;
-        int endIndex = dataArray.length - 1;
-        return binarySearch(startIndex, endIndex, searchData);
+        int endIndex = table.length - 1;
+        return binarySearch(startIndex, endIndex, data);
     }
 
-    private T binarySearch(int startIndex, int endIndex, T target){
-        printArray(startIndex, endIndex);
+    private T binarySearch(int startIndex, int endIndex, T data){
+        printTable(startIndex, endIndex);
         if(startIndex > endIndex) return null;
         int midIndex = Math.floorDiv((startIndex + endIndex), 2);
-        if(dataArray[midIndex].equals(target)) return dataArray[midIndex];
-        if(dataArray[midIndex].compareTo(target) > 0) endIndex = midIndex - 1;
-        else if(dataArray[midIndex].compareTo(target) < 0) startIndex = midIndex + 1;
-        return binarySearch(startIndex, endIndex, target);
+        if(table[midIndex].equals(data)) return table[midIndex];
+        if(table[midIndex].compareTo(data) > 0) endIndex = midIndex - 1;
+        else if(table[midIndex].compareTo(data) < 0) startIndex = midIndex + 1;
+        return binarySearch(startIndex, endIndex, data);
     }
 
-    private void printArray(int startIndex, int endIndex){
+    private void printTable(int startIndex, int endIndex){
         if(isEmpty()) return;
         System.out.print("Current Array: [ ");
         for(int i = startIndex; i <= endIndex; i++)
-            System.out.print(dataArray[i] + " ");
+            System.out.print(table[i] + " ");
         System.out.println("]\n");
     }
-
-
 }
