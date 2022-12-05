4bce9746a6b8802fab0fd9d4d98625e81c8e46ad
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-12-05 05:23:44 -0600

    Completed Helper Methods For Binary Search

--- src/main/java/BinarySearch/BinarySearch.java
+++ src/main/java/BinarySearch/BinarySearch.java
@@ -12,4 +12,18 @@ public class BinarySearch<T extends Comparable<T>>{
     private boolean isEmpty(){ return size == 0; }
     private boolean isFull(){ return size == dataArray.length; }
     private boolean isNull(Object obj){ return obj == null; }
+
+    public void insertData(T newData){
+        if(isNull(newData)) throw new IllegalArgumentException("Data Is Null");
+        if(isFull()) return;
+        dataArray[size++] = newData;
+    }
+
+    private void printArray(int startIndex, int endIndex){
+        if(isEmpty()) return;
+        System.out.print("Current Array Iteration: [");
+        for(int i = startIndex; i <= endIndex; i++)
+            System.out.print(dataArray[i] + " ");
+        System.out.println("]\n");
+    }
 }
