7392165057e7529d565bf5d5a0e95dcec2bf1247
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-12-05 13:17:51 -0600

    Completed Deque

--- src/main/java/Deque/PracticeDeque.java
+++ src/main/java/Deque/PracticeDeque.java
@@ -3,29 +3,25 @@ package Deque;
 public class PracticeDeque<T>{
     private int front;
     private int rear;
-    private int size;
     private int capacity;
+    private int size;
     private T[] deque;
 
     public PracticeDeque(int capacity){
         size = 0;
         front = rear = -1;
         this.capacity = capacity;
-        deque = (T[]) new Object[capacity];
+        this.deque = (T[]) new Object[capacity];
     }
 
-    private boolean isEmpty(){ return (front == -1 && rear == -1) && (size == 0); }
-
-    private boolean isFull(){ return (front == 0 && rear == capacity - 1) || (front == rear + 1); }
-
+    private boolean isEmpty(){ return front == -1 && rear == -1; }
+    private boolean isFull(){ return front == 0 && rear == capacity - 1 || front == rear + 1; }
+    private boolean isNull(Object obj){ return obj == null; }
     private boolean isFrontRearSameIndex(){ return front == rear; }
-
     private void setFrontRearIndex(int index){ front = rear = index; }
 
-    private boolean isNull(Object obj) { return obj == null; }
-
     public boolean insertFront(T newData){
-        if(isNull(newData)) throw new IllegalArgumentException("Data Is Empty!");
+        if(isNull(newData)) throw new IllegalArgumentException("Data Is Null");
         if(isFull()) return false;
         if(isEmpty()) setFrontRearIndex(0);
         else if(front == 0) front = capacity - 1;
@@ -36,7 +32,7 @@ public class PracticeDeque<T>{
     }
 
     public boolean insertRear(T newData){
-        if(isNull(newData)) throw new IllegalArgumentException("Data Is Empty!");
+        if(isNull(newData)) throw new IllegalArgumentException("Data Is Null");
         if(isFull()) return false;
         if(isEmpty()) setFrontRearIndex(0);
         else if(rear == capacity - 1) rear = 0;
@@ -48,24 +44,24 @@ public class PracticeDeque<T>{
 
     public T removeFront(){
         if(isEmpty()) return null;
-        T frontData = deque[front];
+        T removedFrontData = deque[front];
         deque[front] = null;
         if(isFrontRearSameIndex()) setFrontRearIndex(-1);
         else if(front == capacity - 1) front = 0;
         else front++;
         size--;
-        return frontData;
+        return removedFrontData;
     }
 
     public T removeRear(){
         if(isEmpty()) return null;
-        T rearData = deque[rear];
+        T removedRearData = deque[rear];
         deque[rear] = null;
         if(isFrontRearSameIndex()) setFrontRearIndex(-1);
         else if(rear == 0) rear = capacity - 1;
         else rear--;
         size--;
-        return rearData;
+        return removedRearData;
     }
 
     public T viewFront(){
