737190487cec074bb6b0d380d4bfb59bc11cd3e8
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-12-12 02:06:21 -0600

    Completed Traversal Methods for AVL

--- src/main/java/AVL/AVL.java
+++ src/main/java/AVL/AVL.java
@@ -9,4 +9,28 @@ public class AVL<T extends Comparable<T>>{
     private boolean isNull(Object obj){ return obj == null; }
     private boolean isLeftSide(T newData, T data){ return data.compareTo(newData) > 0; }
     private boolean isRightSide(T newData, T data){ return data.compareTo(newData) < 0; }
+
+    public void preOrderTraversal(AVLNode<T> focusNode){
+        if(!isNull(focusNode)){
+            System.out.println(focusNode.getData());
+            preOrderTraversal(focusNode.getLeftSide());
+            preOrderTraversal(focusNode.getRightSide());
+        }
+    }
+
+    public void inOrderTraversal(AVLNode<T> focusNode){
+        if(!isNull(focusNode)){
+            inOrderTraversal(focusNode.getLeftSide());
+            System.out.println(focusNode.getData());
+            inOrderTraversal(focusNode.getRightSide());
+        }
+    }
+
+    public void postOrderTraversal(AVLNode<T> focusNode){
+        if(!isNull(focusNode)){
+            postOrderTraversal(focusNode.getLeftSide());
+            postOrderTraversal(focusNode.getRightSide());
+            System.out.println(focusNode.getData());
+        }
+    }
 }
