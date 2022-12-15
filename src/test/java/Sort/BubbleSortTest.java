e3b7b5c98c00784ba7a2bd85498053560bac89b2
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-12-15 04:25:12 -0600

    Completed Bubble Sort Test

---
+++ src/test/java/Sort/BubbleSortTest.java
@@ -0,0 +1,47 @@
+package Sort;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BubbleSortTest {
+
+    private BubbleSort<Integer> underTest;
+
+    @BeforeEach
+    public void setUp(){
+        int cap = 15;
+        underTest = new BubbleSort<>(cap);
+    }
+
+    @Test
+    public void insertTest(){
+        for(int i = 0; i < underTest.getCapacity(); i++){
+            int randomNumber = new Random().nextInt(0, 30);
+            underTest.insert(randomNumber);
+        }
+        underTest.printArray();
+    }
+
+    @Test
+    public void insertTest2(){
+        Integer randomNum = null;
+        assertThrows(IllegalArgumentException.class, () -> underTest.insert(randomNum));
+    }
+
+    @Test
+    public void sortTest(){
+        for(int i = 0; i < underTest.getCapacity(); i++){
+            int randomNumber = new Random().nextInt(0, 20);
+            underTest.insert(randomNumber);
+        }
+        System.out.print("Before: ");
+        underTest.printArray();
+        underTest.sort();
+        System.out.print("\n\nAfter: ");
+        underTest.printArray();
+    }
+}
