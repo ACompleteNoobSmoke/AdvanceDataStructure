de52a0c01211d3e50811435964d0aea7a5c3da19
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-12-21 06:43:13 -0600

    Completed Selection Sort

--- src/main/java/Sort/SelectionSort.java
+++ src/main/java/Sort/SelectionSort.java
@@ -1,18 +1,17 @@
 package Sort;
 
-public class SelectionSort<T extends Comparable<T>> extends Sorting<T> {
+public class SelectionSort<T extends Comparable<T>> extends Sorting<T>{
     public SelectionSort(int capacity){ super(capacity); }
 
     @Override
-    public void sort() {
-        int minIndex = 0;
+    public void sort(){
         for(int i = 0; i < dataArray.length - 1; i++){
-            minIndex = i;
+            int minIndex = i;
             for(int j = i + 1; j < dataArray.length; j++){
                 if(dataArray[j].compareTo(dataArray[minIndex]) < 0)
                     minIndex = j;
             }
-            if(minIndex != i) swap(i, minIndex);
+            if(i != minIndex) swap(i, minIndex);
         }
     }
 }
