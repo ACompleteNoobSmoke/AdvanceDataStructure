2cf4b50a02ed36230123016d90f89985074d811f
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-12-22 01:22:47 -0600

    Completed Bubble Sort

--- src/main/java/Sort/BubbleSort.java
+++ src/main/java/Sort/BubbleSort.java
@@ -1,15 +1,19 @@
 package Sort;
 
 public class BubbleSort<T extends Comparable<T>> extends Sorting<T>{
-    public BubbleSort(int capacity){ super(capacity); }
+    public BubbleSort(int capacity){
+        super(capacity);
+    }
 
     @Override
     public void sort(){
         for(int i = 0; i < dataArray.length - 1; i++){
             boolean isSwapped = false;
             for(int j = 0; j < dataArray.length - i - 1; j++){
-                if(dataArray[j].compareTo(dataArray[j + 1]) > 0)
-                    swap(j, j+1); isSwapped = true;
+                if(dataArray[j].compareTo(dataArray[j + 1]) > 0){
+                    swap(j, j+1);
+                    isSwapped = true;
+                }
             }
             if(!isSwapped) break;
         }
