771a52530e987670202b4b31de8effee535df82c
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2023-01-02 03:43:40 -0600

    Completed Bubble, Insertion & Selection Sort

--- src/main/java/Sort/BubbleSort.java
+++ src/main/java/Sort/BubbleSort.java
@@ -1,14 +1,16 @@
 package Sort;
 
 public class BubbleSort<T extends Comparable<T>> extends Sorting<T>{
-    public BubbleSort(int capacity){ super(capacity); }
+    public BubbleSort(int cap){
+        super(cap);
+    }
 
     @Override
     public void sort(){
         for(int i = 0; i < dataArray.length - 1; i++){
             boolean isSwapped = false;
             for(int j = 0; j < dataArray.length - i - 1; j++){
-                if(dataArray[j].compareTo(dataArray[j+1]) > 0)
+                if(dataArray[j].compareTo(dataArray[j + 1]) > 0)
                     swap(j, j+1);
                     isSwapped = true;
             }
--- src/main/java/Sort/SelectionSort.java
+++ src/main/java/Sort/SelectionSort.java
@@ -1,11 +1,13 @@
 package Sort;
 
 public class SelectionSort<T extends Comparable<T>> extends Sorting<T>{
-    public SelectionSort(int capacity){ super(capacity); }
+    public SelectionSort(int cap){
+        super(cap);
+    }
 
     @Override
     public void sort(){
-        for(int i = 0; i < dataArray.length - 1; i++){
+        for(int i = 0; i < dataArray.length-1; i++){
             int minIndex = i;
             for(int j = i + 1; j < dataArray.length; j++){
                 if(dataArray[j].compareTo(dataArray[minIndex]) < 0)
