03316afe4bd9686ae63587b35368da86ea2036be
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2023-01-09 02:42:25 -0600

    First version of Merge Sort Test

---
+++ src/test/java/Sort/MergeSortTest.java
@@ -0,0 +1,47 @@
+package Sort;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class MergeSortTest {
+    private MergeSort<Integer> underTest;
+
+    @BeforeEach
+    public void setUp(){
+        int cap = 20;
+        underTest = new MergeSort<>(cap);
+    }
+
+    @Test
+    public void insertTest(){
+        for(int i = 0; i < underTest.getCapacity(); i++){
+            int randomNumber = new Random().nextInt(0, 30);
+            underTest.insert(randomNumber);
+        }
+        underTest.printArray();
+        System.out.println("\n");
+    }
+
+    @Test
+    public void insertTest2(){
+        Integer randomNum = null;
+        assertThrows(IllegalArgumentException.class, () -> underTest.insert(randomNum));
+    }
+
+    @Test
+    public void sortTest(){
+        for(int i = 0; i < underTest.getCapacity(); i++){
+            int randomNumber = new Random().nextInt(0, 31);
+            underTest.insert(randomNumber);
+        }
+        System.out.print("Before: ");
+        underTest.printArray();
+        underTest.sort();
+        System.out.print("\n\nAfter: ");
+        underTest.printArray();
+    }
+}
