c8a2bded7667d1ea3a64134eabdef3a7fb25c550
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2023-01-25 06:35:50 -0600

    Completed Radix Sort

--- src/main/java/Sort/CountingSort.java
+++ src/main/java/Sort/CountingSort.java
@@ -19,30 +19,39 @@ public class CountingSort{
         return (max - min) + 1;
     }
 
-    private void countSort(int[] inputArray){
-        int min = Arrays.stream(inputArray).min().orElse(0);
-        int max = Arrays.stream(inputArray).max().orElse(Integer.MAX_VALUE);
-        int countLen = getCountLength(min, max);
+    protected int getMin(int[] minArray){
+        return Arrays.stream(minArray).min().orElse(0);
+    }
 
-        int[] countArray = new int[countLen];
+    protected int getMax(int[] maxArray){
+        return Arrays.stream(maxArray).max().orElse(Integer.MAX_VALUE);
+    }
+
+    private void countSort(int[] inputArray){
+        int min = getMin(inputArray);
+        int max = getMax(inputArray);
+        int countLength = getCountLength(min, max);
+
+        int[] countArray = new int[countLength];
         for(int input: inputArray) countArray[input - min]++;
 
         int arrayIndex = 0;
-        for(int i = 0; i < countLen; i++)
+        for(int i = 0; i < countLength; i++)
             while(countArray[i] > 0){
                 inputArray[arrayIndex++] = i + min;
                 countArray[i]--;
             }
+
     }
 
     private void countSort(int[] inputArray, int length){
-        int min = Arrays.stream(inputArray).min().orElse(0);
-        int max = Arrays.stream(inputArray).max().orElse(Integer.MAX_VALUE);
-        int countLen = getCountLength(min, max);
+        int min = getMin(inputArray);
+        int max = getMax(inputArray);
+        int countLength = getCountLength(min, max);
 
-        int[] countArray = new int[countLen];
+        int[] countArray = new int[countLength];
         for(int input: inputArray) countArray[input - min]++;
-        for(int i = 1; i < countLen; i++) countArray[i] += countArray[i - 1];
+        for(int i = 1; i < countLength; i++) countArray[i] += countArray[i - 1];
 
         int[] outputArray = new int[length];
         for(int i = length - 1; i >= 0; i--){
--- src/main/java/Sort/RadixSort.java
+++ src/main/java/Sort/RadixSort.java
@@ -10,28 +10,28 @@ public class RadixSort extends CountingSort{
 
     @Override
     public void sort(){
-        int max = Arrays.stream(array).max().orElse(Integer.MAX_VALUE);
+        int max = getMax(array);
         for(int exp = 1; max / exp > 0; exp *= 10) countSort(exp);
     }
 
     private void countSort(int exp){
-        int len = 10;
-        int[] countArray = new int[len];
+        int fixedLen = 10;
+        int[] countArray = new int[fixedLen];
 
-        for(int value: array) countArray[(value / exp) % len]++;
-        for(int i = 1; i < len; i++) countArray[i] += countArray[i - 1];
+        for(int value: array) countArray[(value / exp) % fixedLen]++;
+        for(int i = 1; i < fixedLen; i++) countArray[i] += countArray[i - 1];
 
-        int arrayLength = array.length;
-        int[] outputArray = new int[arrayLength];
-        for(int i = arrayLength - 1; i >= 0; i--){
+        int arrLen = array.length;
+        int[] outputArray = new int[arrLen];
+        for(int i = arrLen - 1; i >= 0; i--){
             int currentValue = array[i];
-            int countArrayIndex = (currentValue / exp) % len;
+            int countArrayIndex = (currentValue / exp) % fixedLen;
             int positionInArray = countArray[countArrayIndex] - 1;
             outputArray[positionInArray] = currentValue;
             countArray[countArrayIndex]--;
         }
 
-        System.arraycopy(outputArray, 0, array, 0, arrayLength);
+        System.arraycopy(outputArray, 0, array, 0, arrLen);
     }
 }
 
