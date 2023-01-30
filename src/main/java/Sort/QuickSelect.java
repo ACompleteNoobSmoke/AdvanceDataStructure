353960c68fc823765e0497088b85df629aa8b014
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2023-01-30 06:58:43 -0600

    Quick Select

--- src/main/java/Sort/QuickSelect.java
+++ src/main/java/Sort/QuickSelect.java
@@ -4,19 +4,17 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class QuickSelect<T extends Comparable<T>> extends QuickSort<T> {
+    public QuickSelect(int capacity){ super(capacity); }
+
     private T answer;
     private int k;
-
-    public QuickSelect(int capacity) {
-        super(capacity);
-    }
 
     public T getAnswer(){ return answer; }
     public int getK(){ return k; }
 
     @Override
     public void sort(){
-        int arrayLength = dataArray.length - 1;
+        int arrayLength = capacity - 1;
         k = new Random().nextInt(0, arrayLength);
         answer = quickSelect(dataArray, k, 0, arrayLength);
         Arrays.sort(dataArray);
@@ -24,8 +22,8 @@ public class QuickSelect<T extends Comparable<T>> extends QuickSort<T> {
 
     private T quickSelect(T[] inputArray, int k, int low, int high){
         int partition = getPartition(inputArray, low, high);
-        if(partition == k - 1) return inputArray[partition];
-        else if(partition < k - 1) low = partition + 1;
+        if (partition == k - 1) return inputArray[partition];
+        else if (partition < k - 1) low = partition + 1;
         else high = partition - 1;
         return quickSelect(inputArray, k, low, high);
     }
@@ -34,9 +32,10 @@ public class QuickSelect<T extends Comparable<T>> extends QuickSort<T> {
         T pivotElement = inputArray[high];
         int pivotIndex = low;
 
-        for(int i = low; i < high; i++)
+        for(int i = low; i < high; i++){
             if(inputArray[i].compareTo(pivotElement) < 0)
                 swap(i, pivotIndex++);
+        }
 
         swap(pivotIndex, high);
         return pivotIndex;
