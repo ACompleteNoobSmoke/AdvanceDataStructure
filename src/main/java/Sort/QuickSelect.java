83349efb57e3b404892dea831f5073bbb1322cbc
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2023-01-31 05:55:57 -0600

    Quick Select

--- src/main/java/Sort/QuickSelect.java
+++ src/main/java/Sort/QuickSelect.java
@@ -4,40 +4,48 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class QuickSelect<T extends Comparable<T>> extends QuickSort<T> {
-    public QuickSelect(int capacity){ super(capacity); }
+   private T kthElement;
+   private int kthIndex;
 
-    private T answer;
-    private int k;
+   public QuickSelect(int capacity){
+       super(capacity);
+   }
 
-    public T getAnswer(){ return answer; }
-    public int getK(){ return k; }
+   public T getAnswer(){
+       return kthElement;
+   }
 
-    @Override
+   public int getK(){
+       return kthIndex;
+   }
+
+   @Override
     public void sort(){
-        int arrayLength = capacity - 1;
-        k = new Random().nextInt(0, arrayLength);
-        answer = quickSelect(dataArray, k, 0, arrayLength);
+        int arrayLength = dataArray.length - 1;
+        kthIndex = new Random().nextInt(0, arrayLength);
+        kthElement = quickSelect(dataArray, kthIndex, 0, arrayLength);
         Arrays.sort(dataArray);
-    }
+   }
 
-    private T quickSelect(T[] inputArray, int k, int low, int high){
+   private T quickSelect(T[] inputArray, int k, int low, int high){
         int partition = getPartition(inputArray, low, high);
         if (partition == k - 1) return inputArray[partition];
         else if (partition < k - 1) low = partition + 1;
         else high = partition - 1;
         return quickSelect(inputArray, k, low, high);
-    }
+   }
 
-    private int getPartition(T[] inputArray, int low, int high){
+   private int getPartition(T[] inputArray, int low, int high){
         T pivotElement = inputArray[high];
         int pivotIndex = low;
 
         for(int i = low; i < high; i++){
-            if(inputArray[i].compareTo(pivotElement) < 0)
+            if(inputArray[i].compareTo(pivotElement) < 0){
                 swap(i, pivotIndex++);
+            }
         }
 
         swap(pivotIndex, high);
         return pivotIndex;
-    }
+   }
 }
