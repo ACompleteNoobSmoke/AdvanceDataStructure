6b21a08e5907b318e6d77e3fa3e84adeec555a3c
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2023-02-26 09:17:44 -0600

    Completed Substring Values Method of KMP Algorithm

--- src/main/java/PatternMatching/KMP.java
+++ src/main/java/PatternMatching/KMP.java
@@ -4,20 +4,17 @@ public class KMP {
 
     public int[] getSubstringValues(String pattern){
         int lengthOfPattern = pattern.length();
-        int[] values = new int[lengthOfPattern];
         int leftPointer = 0, rightPointer = 1;
+        int[] values = new int[lengthOfPattern];
         values[leftPointer] = leftPointer;
 
         while (rightPointer < lengthOfPattern){
             char leftChar = pattern.charAt(leftPointer);
             char rightChar = pattern.charAt(rightPointer);
-            if (leftChar == rightChar) values[rightPointer] = ++leftPointer;
-            else if (leftPointer == 0) values[rightPointer] = 0;
-            else{
-                leftPointer = values[leftPointer - 1];
-                rightPointer--;
-            }
-            rightPointer++;
+            if (leftChar == rightChar){ values[rightPointer] = leftPointer++ + 1; }
+            else if(leftPointer == 0) values[rightPointer] = 0;
+            else { leftPointer = values[leftPointer - 1]; rightPointer--;}
+            ++rightPointer;
         }
 
         return values;
