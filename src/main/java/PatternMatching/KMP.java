db09fd8403968130e4e973cb8c6b6dbd234a3816
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2023-02-27 06:22:23 -0600

    Completed My Version of KMP Algorithm

--- src/main/java/PatternMatching/KMP.java
+++ src/main/java/PatternMatching/KMP.java
@@ -4,39 +4,41 @@ public class KMP {
 
     public int[] getSubstringValues(String pattern){
         int lengthOfPattern = pattern.length();
-        int leftPointer = 0, rightPointer = 1;
         int[] values = new int[lengthOfPattern];
+        int leftPointer = 0, rightPointer = 1;
         values[leftPointer] = leftPointer;
 
         while (rightPointer < lengthOfPattern){
-            char leftChar = pattern.charAt(leftPointer);
-            char rightChar = pattern.charAt(rightPointer);
-            if (leftChar == rightChar){ values[rightPointer] = leftPointer++ + 1; }
-            else if(leftPointer == 0) values[rightPointer] = 0;
-            else { leftPointer = values[leftPointer - 1]; rightPointer--;}
-            ++rightPointer;
+            char leftCharacter = pattern.charAt(leftPointer);
+            char rightCharacter = pattern.charAt(rightPointer);
+            if (leftCharacter == rightCharacter) values[rightPointer] = leftPointer++ + 1;
+            else if (leftPointer == 0) values[rightPointer] = 0;
+            else{
+                leftPointer = values[leftPointer - 1];
+                continue;
+            }
+            rightPointer++;
         }
 
         return values;
     }
 
     public boolean searchPattern(String pattern, String text){
-        int[] subStringValues = getSubstringValues(pattern);
         int lengthOfPattern = pattern.length();
         int lengthOfText = text.length();
-        int textPointer = 0, patternPointer = 0;
-        int counter = 0;
+        int[] values = getSubstringValues(pattern);
+        int patternPointer = 0, textPointer = 0, counter = 0;
 
-        while (textPointer < lengthOfText && patternPointer < lengthOfPattern){
-            char patternChar = pattern.charAt(patternPointer++);
-            char textChar = text.charAt(textPointer++);
-            if (patternChar != textChar) {
-                patternPointer = (patternPointer - 1 <= 0) ? 0 : subStringValues[patternPointer - 2];
+        while (textPointer < lengthOfText){
+            char patternCharacter = pattern.charAt(patternPointer++);
+            char textCharacter = text.charAt(textPointer++);
+            if (patternCharacter != textCharacter){
+                patternPointer = (patternPointer - 1 <= 0) ? 0 : values[patternPointer - 2];
                 counter = patternPointer;
-            } else counter++;
-
+            }else counter++;
             if (counter == lengthOfPattern) return true;
         }
+
         return false;
     }
 }
