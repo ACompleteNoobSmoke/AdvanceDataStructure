f4c0cd5a8fa74a40db2d8079ccedb7b2bc067bd6
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2023-03-09 02:19:31 -0600

    Working on Graph Theory

--- src/main/java/GraphTheory/Graph.java
+++ src/main/java/GraphTheory/Graph.java
@@ -1,9 +1,39 @@
 package GraphTheory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Graph {
+    List<Vertex> vertices;
     int[][] matrix;
 
     public Graph(int size){
         matrix = new int[size][size];
+        vertices = new ArrayList<>();
+    }
+
+    public void addVertex(Vertex node){
+        vertices.add(node);
+    }
+
+    public void addEdge(int src, int dest){
+        matrix[src][dest] = 1;
+    }
+
+    public boolean checkEdge(int src, int dest){
+        return matrix[src][dest] == 1;
+    }
+
+    public void print(){
+        System.out.print("  ");
+        vertices.forEach(n -> System.out.print(n.getData() + " "));
+        System.out.println();
+        for (int i = 0; i < matrix.length; i++){
+            System.out.print(vertices.get(i).getData() + " ");
+            for (int j = 0; j < matrix[i].length; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
