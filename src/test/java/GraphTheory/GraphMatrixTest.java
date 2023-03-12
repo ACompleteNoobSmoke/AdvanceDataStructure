ae7250bd69b10b83c7eccba034d53971c1d6b654
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2023-03-12 12:08:34 -0500

    Testing Graph Matrix

---
+++ src/test/java/GraphTheory/GraphMatrixTest.java
@@ -0,0 +1,46 @@
+package GraphTheory;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+
+public class GraphMatrixTest {
+    private GraphMatrix<Character> underTest;
+
+    @Test
+    public void graphTest1(){
+        Vertex<Character> vertex1 = new Vertex<>('A', 0);
+        Vertex<Character> vertex2 = new Vertex<>('B', 1);
+        Vertex<Character> vertex3 = new Vertex<>('C', 2);
+        Vertex<Character> vertex4 = new Vertex<>('D', 3);
+        Vertex<Character> vertex5 = new Vertex<>('E', 4);
+        Vertex<Character> vertex6 = new Vertex<>('F', 5);
+
+        Edge<Character> edge1 = new Edge<>(vertex1, vertex2);
+        Edge<Character> edge2 = new Edge<>(vertex2, vertex6);
+        Edge<Character> edge3 = new Edge<>(vertex3, vertex1);
+        Edge<Character> edge4 = new Edge<>(vertex4, vertex2);
+        Edge<Character> edge5 = new Edge<>(vertex5, vertex4);
+        Edge<Character> edge6 = new Edge<>(vertex6, vertex3);
+
+        underTest = new GraphMatrix<>(new HashSet<Vertex>(), new HashSet<Edge>(), 6);
+        underTest.addVertices(vertex1);
+        underTest.addVertices(vertex2);
+        underTest.addVertices(vertex3);
+        underTest.addVertices(vertex4);
+        underTest.addVertices(vertex5);
+        underTest.addVertices(vertex6);
+
+        underTest.addEdge(edge1);
+        underTest.addEdge(edge2);
+        underTest.addEdge(edge3);
+        underTest.addEdge(edge4);
+        underTest.addEdge(edge5);
+        underTest.addEdge(edge6);
+
+        underTest.print();
+
+
+
+    }
+}
