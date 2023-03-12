c13ca92ff7e6719169f545a1705aad1da0efc124
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2023-03-12 12:08:04 -0500

    Made new Graph class -- very rough!

--- src/main/java/GraphTheory/GraphMatrix.java
+++ src/main/java/GraphTheory/GraphMatrix.java
@@ -1,47 +1,54 @@
 package GraphTheory;
 
-import java.util.Comparator;
-import java.util.Set;
+import java.util.*;
 
 public class GraphMatrix<T> implements Comparator<Edge<? super  T>> {
+    List<T> data;
     private Set<Vertex<T>> vertices;
     private Set<Edge<T>> edges;
     private int[][] matrix;
 
     public GraphMatrix(Set vertices, Set edges, int size){
+        data = new ArrayList<>();
         this.vertices = vertices;
         this.edges = edges;
         matrix = new int[size][size];
     }
 
-    public void addVertices(Vertex newVertex){
+    public void addVertices(Vertex<T> newVertex){
         vertices.add(newVertex);
+        data.add(newVertex.getData());
     }
 
-    public void addEdge(Edge newEdge){
+    public void addEdge(Edge<T> newEdge){
         edges.add(newEdge);
         if(!checkEdge(newEdge))
             addEdgeToMatrix(newEdge);
     }
 
-    private void addEdgeToMatrix(Edge newEdge){
+    private void addEdgeToMatrix(Edge<T> newEdge){
         matrix[newEdge.getStartPoint()][newEdge.getEndPoint()] = 1;
     }
 
-    public boolean checkEdge(Edge checkThis){
+    public boolean checkEdge(Edge<T> checkThis){
         return matrix[checkThis.getStartPoint()][checkThis.getEndPoint()] == 1;
     }
 
     public void print(){
         System.out.print("  ");
-        vertices.forEach(v -> System.out.print(v.getData() + " "));
+        //vertices.stream().sorted().forEach(v -> System.out.print(v.getData() + " "));
+        Comparator<Vertex<T>> comp = new Comparator<Vertex<T>>() {
+            @Override
+            public int compare(Vertex<T> o1, Vertex<T> o2) {
+                return o1.getPoint() - o2.getPoint();
+            }
+        };
+        vertices.stream().sorted(comp).forEach(v -> System.out.print(v.getData() + " "));
         System.out.println();
 
-        T[] vertArr =  (T[])new Object[vertices.size()];
-        vertArr = vertices.toArray(vertArr);
 
         for (int i = 0; i < matrix.length; i++){
-            System.out.print(vertArr[i] + " ");
+            System.out.print(data.get(i) + " ");
             for (int j = 0; j < matrix[i].length; j++){
                 System.out.print(matrix[i][j] + " ");
             }
--- src/main/java/GraphTheory/Vertex.java
+++ src/main/java/GraphTheory/Vertex.java
@@ -3,12 +3,17 @@ package GraphTheory;
 import lombok.Data;
 
 @Data
-public class Vertex<T> {
+public class Vertex<T> implements Comparable<T> {
     private T data;
     private int point;
     public Vertex(T data, int point){
         if (data == null){ throw new IllegalArgumentException("Data Is Null"); }
         this.data = data;
         this.point = point;
+    }
+
+    @Override
+    public int compareTo(T o) {
+        return this.point - getPoint();
     }
 }
