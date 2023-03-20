ea07ee2424029c88f8f25db276ce46f766665ca5
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2023-03-20 11:01:07 -0500

    Cleaned up Graph List & Included DFS Method

--- src/main/java/GraphTheory/GraphList.java
+++ src/main/java/GraphTheory/GraphList.java
@@ -1,76 +1,78 @@
 package GraphTheory;
 
-import javax.xml.crypto.Data;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class GraphList<T> extends Graph<T>{
-    private final List<LinkedList<Vertex<T>>> listOfData;
+    private List<LinkedList<Vertex<T>>> listOfVertices;
 
     public GraphList(Set<Vertex<T>> vertexSet, Set<Edge<T>> edges){
         super(vertexSet, edges);
-        this.listOfData = new ArrayList<>();
+        listOfVertices = new ArrayList<>();
     }
 
     @Override
     public void addVertices(Vertex<T> newVertex){
         if (!vertices.add(newVertex)) return;
-        LinkedList<Vertex<T>> newList = new LinkedList<>();
-        newList.add(newVertex);
-        listOfData.add(newVertex.getGraphPoint(), newList);
+        LinkedList<Vertex<T>> currentList = new LinkedList<Vertex<T>>();
+        currentList.add(newVertex);
+        listOfVertices.add(newVertex.getGraphPoint(), currentList);
     }
 
     @Override
     public void addEdge(Edge<T> newEdge){
         if (!edges.add(newEdge)) return;
-        LinkedList<Vertex<T>> currentList = listOfData.get(newEdge.getStartVertex().getGraphPoint());
-        Vertex<T> tail = listOfData.get(newEdge.getEndVertex().getGraphPoint()).get(0);
+        int src = newEdge.getStartVertex().getGraphPoint();
+        int dest = newEdge.getEndVertex().getGraphPoint();
+        addEdgeToLinkedList(src, dest);
+    }
+
+    private void addEdgeToLinkedList(int src, int dest){
+        LinkedList<Vertex<T>> currentList = listOfVertices.get(src);
+        Vertex<T> tail = listOfVertices.get(dest).get(0);
         currentList.addLast(tail);
     }
 
     @Override
     public boolean checkEdge(int src, int dest){
-        System.out.println(src);
-        LinkedList<Vertex<T>> currentList = listOfData.get(src);
+        LinkedList<Vertex<T>> currentList = listOfVertices.get(src);
         if (currentList == null) return false;
-        for (Vertex<T> vert: currentList)
+        for (Vertex<T> vert : currentList){
             if (vert.getGraphPoint() == dest) return true;
+        }
         return false;
     }
+
 
     public void DFS(T src){
         Vertex<T> currentVertex = getVertex(src);
         if (currentVertex == null) return;
-        int srcPoint = currentVertex.getGraphPoint();
-        DFSHelper(srcPoint, new boolean[listOfData.size()]);
+        DFSHelper(currentVertex.getGraphPoint(), new HashSet<>());
     }
 
-    private void DFSHelper(int srcPoint, boolean[] visited){
-        if (visited[srcPoint]) return;
-        else{
-            visited[srcPoint] = true;
-            System.out.println(listOfData.get(srcPoint).get(0).getData() + ": Visited!");
-        }
-
-        for (Vertex<T> vert: listOfData.get(srcPoint)){
+    private void DFSHelper(int src, Set<Integer> visited){
+        if (!visited.add(src)) return;
+        LinkedList<Vertex<T>> current = listOfVertices.get(src);
+        System.out.println(current.get(0).getData() + ": Visited!");
+        for (Vertex<T> vert: current)
             DFSHelper(vert.getGraphPoint(), visited);
-        }
     }
 
     private Vertex<T> getVertex(T src){
-        for (Vertex<T> vertex: vertices)
-            if (vertex.getData().equals(src)) return vertex;
+        for (Vertex<T> vert : vertices)
+            if (vert.getData().equals(src)) return vert;
         return null;
+    }
+
+    private List<Vertex<T>> getListOfVertex(){
+        return vertices.stream().sorted(comparator).toList();
     }
 
     @Override
     public void print(){
-        for (LinkedList<Vertex<T>> listOfDatum : listOfData) {
-            for (int j = 0; j < listOfDatum.size(); j++) {
-                System.out.print(listOfDatum.get(j).getData());
-                if (j != listOfDatum.size() - 1) System.out.print(" -> ");
+        for (LinkedList<Vertex<T>> vert : listOfVertices){
+            for (int i = 1; i <= vert.size(); i++){
+                System.out.print(vert.get(i).getData());
+                if (i != vert.size()) System.out.print(" -> ");
             }
             System.out.println();
         }
--- src/main/java/GraphTheory/GraphMatrix.java
+++ src/main/java/GraphTheory/GraphMatrix.java
@@ -4,10 +4,9 @@ import java.util.List;
 import java.util.Set;
 
 public class GraphMatrix<T> extends Graph<T>{
-    private final int[][] matrix;
-
-    public GraphMatrix(Set<Vertex<T>> vertices, Set<Edge<T>> edges, int size){
-        super(vertices, edges);
+    private int[][] matrix;
+    public GraphMatrix(Set<Vertex<T>> vertexSet, Set<Edge<T>> edgeSet, int size){
+        super(vertexSet, edgeSet);
         this.matrix = new int[size][size];
     }
 
@@ -20,47 +19,42 @@ public class GraphMatrix<T> extends Graph<T>{
     }
 
     private void addEdgeToMatrix(int src, int dest){
-        int matrixLen = matrix.length;
-        if (src >= matrixLen || dest >= matrixLen) return;
         matrix[src][dest] = 1;
     }
 
-    @Override
     public boolean checkEdge(int src, int dest){
-        int matrixLen = matrix.length;
-        if (src >= matrixLen || dest >= matrixLen) return false;
         return matrix[src][dest] == 1;
     }
 
     public void DFS(T src){
-        Vertex<T> current = getVertex(src);
-        if (current ==  null) return;
-        List<Vertex<T>> listOfVertices = getListOfData();
-        int srcPoint = current.getGraphPoint();
-        DFSHelper(srcPoint, new boolean[matrix.length], listOfVertices);
+        Vertex<T> currentVertex = getVertex(src);
+        if (currentVertex == null) return;
+        List<Vertex<T>> vertexList = getListOfVertex();
+        int srcPoint = currentVertex.getGraphPoint();
+        DFSHelper(srcPoint, new boolean[matrix.length], vertexList);
     }
 
-    private void DFSHelper(int srcPoint, boolean[] visited, List<Vertex<T>> listOfVertices) {
-        if (visited[srcPoint]) return;
-        else {
-            visited[srcPoint] = true;
-            System.out.println(listOfVertices.get(srcPoint).getData() + ": Visited!");
+    private void DFSHelper(int src, boolean[] visited, List<Vertex<T>> vertexList){
+        if(visited[src]) return;
+        else{
+            visited[src] = true;
+            System.out.println(vertexList.get(src).getData() + ": Visited!");
         }
 
-        for (int i = 0; i < matrix[srcPoint].length; i++){
-            if (checkEdge(srcPoint, i)) DFSHelper(i, visited, listOfVertices);
+        for (int i = 0; i < matrix[src].length; i++){
+            if (checkEdge(src, i)) DFSHelper(i, visited, vertexList);
         }
     }
 
     @Override
     public void print(){
-        List<Vertex<T>> headers = getListOfData();
+        List<Vertex<T>> vertexList = getListOfVertex();
         System.out.print("  ");
-        headers.forEach(v -> System.out.print(v.getData() + " "));
+        vertexList.forEach(v -> System.out.print(v.getData() + " "));
         System.out.println();
 
         for (int i = 0; i < matrix.length; i++){
-            System.out.print(headers.get(i).getData() + " ");
+            System.out.print(vertexList.get(i).getData() + " ");
             for (int j = 0; j < matrix[i].length; j++){
                 System.out.print(matrix[i][j] + " ");
             }
@@ -68,13 +62,13 @@ public class GraphMatrix<T> extends Graph<T>{
         }
     }
 
-    private List<Vertex<T>> getListOfData(){
-        return vertices.stream().sorted(comparator).toList();
+    private Vertex<T> getVertex(T src){
+        for (Vertex<T> vertex : vertices)
+            if (vertex.getData().equals(src)) return vertex;
+        return null;
     }
 
-    private Vertex<T> getVertex(T src){
-        for (Vertex<T> vert : vertices)
-            if (vert.getData().equals(src)) return vert;
-        return null;
+    private List<Vertex<T>> getListOfVertex(){
+        return vertices.stream().sorted(comparator).toList();
     }
 }
