2b8c6c851e1fb9b19ed096d1edda32ec6672e32c
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2023-03-21 06:27:03 -0500

    Completed BFS on Graph Matrix

--- src/main/java/GraphTheory/GraphMatrix.java
+++ src/main/java/GraphTheory/GraphMatrix.java
@@ -1,18 +1,22 @@
 package GraphTheory;
 
+
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Set;
 
 public class GraphMatrix<T> extends Graph<T>{
-    private int[][] matrix;
-    public GraphMatrix(Set<Vertex<T>> vertexSet, Set<Edge<T>> edgeSet, int size){
-        super(vertexSet, edgeSet);
+    private final int[][] matrix;
+
+    public GraphMatrix(Set<Vertex<T>> vertices, Set<Edge<T>> edges, int size){
+        super(vertices, edges);
         this.matrix = new int[size][size];
     }
 
     @Override
     public void addEdge(Edge<T> newEdge){
-        if (!edges.add(newEdge)) return;
+        if(!edges.add(newEdge)) return;
         int src = newEdge.getStartVertex().getGraphPoint();
         int dest = newEdge.getEndVertex().getGraphPoint();
         addEdgeToMatrix(src, dest);
@@ -22,6 +26,7 @@ public class GraphMatrix<T> extends Graph<T>{
         matrix[src][dest] = 1;
     }
 
+    @Override
     public boolean checkEdge(int src, int dest){
         return matrix[src][dest] == 1;
     }
@@ -29,32 +34,53 @@ public class GraphMatrix<T> extends Graph<T>{
     public void DFS(T src){
         Vertex<T> currentVertex = getVertex(src);
         if (currentVertex == null) return;
-        List<Vertex<T>> vertexList = getListOfVertex();
-        int srcPoint = currentVertex.getGraphPoint();
-        DFSHelper(srcPoint, new boolean[matrix.length], vertexList);
+        DFSHelper(currentVertex.getGraphPoint(), new boolean[matrix.length], getSortedListOfVertex());
     }
 
-    private void DFSHelper(int src, boolean[] visited, List<Vertex<T>> vertexList){
-        if(visited[src]) return;
+    private void DFSHelper(int srcPoint, boolean[] visited, List<Vertex<T>> vertexList){
+        if(visited[srcPoint]) return;
         else{
-            visited[src] = true;
-            System.out.println(vertexList.get(src).getData() + ": Visited!");
+            visited[srcPoint] = true;
+            System.out.println(vertexList.get(srcPoint).getData() + ": Visited!");
         }
 
-        for (int i = 0; i < matrix[src].length; i++){
-            if (checkEdge(src, i)) DFSHelper(i, visited, vertexList);
+        for (int i = 0; i < matrix[srcPoint].length; i++){
+            if (checkEdge(srcPoint, i)) DFSHelper(i, visited, vertexList);
+        }
+    }
+
+    public void BFS(T src){
+        Vertex<T> currentVertex = getVertex(src);
+        if (currentVertex == null) return;
+
+        Queue<Integer> bfsQ = new LinkedList<>();
+        boolean[] visited = new boolean[matrix.length];
+        bfsQ.offer(currentVertex.getGraphPoint());
+        List<Vertex<T>> sortedList = getSortedListOfVertex();
+
+        while (!bfsQ.isEmpty()){
+            int currentPoint = bfsQ.poll();
+            if(!visited[currentPoint]){
+                visited[currentPoint] = true;
+                System.out.println(sortedList.get(currentPoint).getData() + ": Visited!");
+            }
+
+            for (int i = 0; i < matrix[currentPoint].length; i++){
+                if (checkEdge(currentPoint, i) && !visited[i])
+                    bfsQ.offer(i);
+            }
         }
     }
 
     @Override
     public void print(){
-        List<Vertex<T>> vertexList = getListOfVertex();
+        List<Vertex<T>> header = getSortedListOfVertex();
         System.out.print("  ");
-        vertexList.forEach(v -> System.out.print(v.getData() + " "));
+        header.forEach(v -> System.out.print(v.getData() + " "));
         System.out.println();
 
         for (int i = 0; i < matrix.length; i++){
-            System.out.print(vertexList.get(i).getData() + " ");
+            System.out.print(header.get(i).getData() + " ");
             for (int j = 0; j < matrix[i].length; j++){
                 System.out.print(matrix[i][j] + " ");
             }
@@ -63,12 +89,12 @@ public class GraphMatrix<T> extends Graph<T>{
     }
 
     private Vertex<T> getVertex(T src){
-        for (Vertex<T> vertex : vertices)
-            if (vertex.getData().equals(src)) return vertex;
+        for (Vertex<T> vert: vertices)
+            if (vert.getData().equals(src)) return vert;
         return null;
     }
 
-    private List<Vertex<T>> getListOfVertex(){
+    private List<Vertex<T>> getSortedListOfVertex(){
         return vertices.stream().sorted(comparator).toList();
     }
 }
